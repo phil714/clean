ea6c12b37b6b8879b64e4882329f543014106cfa
Samuel <Scroteau94@gmail.com>
2020-01-19 11:35:18 -0500

    fetch path

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import { Line } from 'react-chartjs-2';
 import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
@@ -12,35 +12,34 @@ import mobilierUrbainGP from '../mobilierurbaingp.json';
 const THRASH_SIZE = 16; // cm;
 
 const data = {
-    datasets: [
-      {
-        label: 'Thrash level over time',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(248,50,47,0.4)',
-        borderColor: 'rgba(248,50,47,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(248,50,47,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 0,
-        pointHoverBackgroundColor: 'rgba(248,50,47,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 1
-      }
-    ]
-  };
+  datasets: [
+    {
+      label: 'Thrash level over time',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(248,50,47,0.4)',
+      borderColor: 'rgba(248,50,47,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(248,50,47,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 0,
+      pointHoverBackgroundColor: 'rgba(248,50,47,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 1
+    }
+  ]
+};
 
+const ACCESS_TOKEN =
+  'pk.eyJ1Ijoic2FtdWVsdHNvIiwiYSI6ImNrNWs3Z2s3YjBiN3Mza241MTkweDNsemEifQ.HAKopF1Q8laHm4hK6NR51A';
 
-const ReactMapboxGlMap = ReactMapboxGl({
-  accessToken:
-    'pk.eyJ1Ijoic2FtdWVsdHNvIiwiYSI6ImNrNWs3Z2s3YjBiN3Mza241MTkweDNsemEifQ.HAKopF1Q8laHm4hK6NR51A'
-});
+const ReactMapboxGlMap = ReactMapboxGl({ accessToken: ACCESS_TOKEN });
 
 firebase.initializeApp({
   apiKey: 'AIzaSyAjyJ-T2RiCOJSFBHEDN3kpBQT3wM39XqY',
@@ -69,6 +68,7 @@ export const Map = () => {
 
   const handleClose = () => setOpen(false);
   const handleOpen = () => setOpen(true);
+  const [directionData, setDirectionData] = useState([]);
 
   useEffect(() => {
     firestore
@@ -107,57 +107,82 @@ export const Map = () => {
   }, []);
 
   const labels = [];
-  for(let i = 0; i < 300; i++) {
-      labels.push(i);
-  }   
+  for (let i = 0; i < 300; i++) {
+    labels.push(i);
+  }
 
-  data.labels = labels;  
-  
+  data.labels = labels;
+
   const currentHeight = polyTrashData[0];
   console.log('currentHeight', currentHeight, 'cm');
 
   const last5minData = _.map(polyTrashData.slice(0, 299), d => {
-      return (THRASH_SIZE - d)/THRASH_SIZE;
-  })
+    return (THRASH_SIZE - d) / THRASH_SIZE;
+  });
 
   data.datasets[0].data = last5minData;
 
-  const currentPercentage = Math.max(0, Math.round(last5minData[0]*100));
+  const currentPercentage = Math.max(0, Math.round(last5minData[0] * 100));
+  useEffect(() => {
+    let coordinatesString = '';
+
+    for (let trash of montrealTrashData.slice(0, 24)) {
+      coordinatesString += trash.Long + ',';
+      coordinatesString += trash.Lat + ';';
+    }
+    coordinatesString = coordinatesString.substring(
+      0,
+      coordinatesString.length - 1
+    );
+
+    fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/cycling/${coordinatesString}?geometries=geojson&access_token=${ACCESS_TOKEN}`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(res => {
+      res.json().then(resJson => {
+        setDirectionData(resJson);
+        console.log('resJson', resJson);
+      });
+    });
+  }, [montrealTrashData]);
 
   return (
     <>
-    <Dialog onClose={handleClose} open={open}>
-      <Line data={data} />
-      <Percentage>{`Current percentage: ${currentPercentage} %`}</Percentage>
-    </Dialog>
-    <ReactMapboxGlMap
-      style="mapbox://styles/mapbox/streets-v11"
-      containerStyle={{
-        height: '100vh',
-        width: '100vw'
-      }}
-      center={[POLY_LONG, POLY_LAT]}
-      zoom={[13]}
-    >
-      <TrashMarker
-        handleOpen={handleOpen}
-        coordinates={[POLY_LONG, POLY_LAT]}
-        color={colors[currentPercentage]}
-        text={`${currentPercentage}%`}
-      />
-      {/* <TrashMarker coordinates={[-73.58781, 45.50884]} color={' #f56565'} />
-      <TrashMarker coordinates={[-73.58781, 45.50984]} color={' #ECC94B'} />
-      <TrashMarker coordinates={[-73.58731, 45.50894]} color={' #48BB78'} /> */}
-      {montrealTrashData.map(trash => (
+      <Dialog onClose={handleClose} open={open}>
+        <Line data={data} />
+        <Percentage>{`Current percentage: ${currentPercentage} %`}</Percentage>
+      </Dialog>
+      <ReactMapboxGlMap
+        style="mapbox://styles/mapbox/streets-v11"
+        containerStyle={{
+          height: '100vh',
+          width: '100vw'
+        }}
+        center={[POLY_LONG, POLY_LAT]}
+        zoom={[13]}
+      >
         <TrashMarker
           handleOpen={handleOpen}
-          key={trash.OBJECTID}
-          coordinates={[trash.Long, trash.Lat]}
-          color={colors[trash.fullness]}
-          text={`${trash.fullness}%`}
+          coordinates={[POLY_LONG, POLY_LAT]}
+          color={colors[currentPercentage]}
+          text={`${currentPercentage}%`}
         />
-      ))}
-    </ReactMapboxGlMap>
+        {montrealTrashData.map(trash => (
+          <TrashMarker
+            handleOpen={handleOpen}
+            key={trash.OBJECTID}
+            coordinates={[trash.Long, trash.Lat]}
+            color={colors[trash.fullness]}
+            text={`${trash.fullness}%`}
+          />
+        ))}
+      </ReactMapboxGlMap>
     </>
   );
 };
