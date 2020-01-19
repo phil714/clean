d957688d3b8882734ff5f054748633def1407f46
phil714 <philippedeschesnes@civalgo.com>
2020-01-18 19:21:45 -0500

    Added Canada311 tile page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Canada311 from './fuck you/Canada311';
+import Map from './fuck you/Map';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Canada311 />
+          </Route>
+          <Route exact path="/map">
+            <Map />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
